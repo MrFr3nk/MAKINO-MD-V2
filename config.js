e4f20a00851fdc2bcbd4df0a4485bf31d7adf917
ᴹᴿ᭄ Fʀᴀɴᴋ ᴏғᴄ 々 <darrelmucheri@gmail.com>
2024-10-06 11:07:23 +0200

    Update config.js

--- config.js
+++ config.js
@@ -18,8 +18,8 @@ global.ANTI_BOT = false;
 global.PM_BLOCKER = false;
 global.CHATBOT = false;
 
-global.SESSION_ID = process.env.SESSION_ID || ""
-global.Owner = ["2349151864541"]; //like 2347080968564 
+global.SESSION_ID = process.env.SESSION_ID || "taira_tech-uhcT1bbI#oMv1kPF90oENHwHf-JMd07egLXFD6bATmwxAsMpZN2M"
+global.Owner = ["263719747303"]; //like 2347080968564 
 global.OwnerName =  "Tᴀɪʀᴀ Mᴀᴋɪɴᴏ";
 global.BotName = "♱MAKINO-MD-V2♱♡⃤";
 global.packname = "♱MAKINO-MD-V2♱♡⃤";                             //Do not change.
